22af83940678baf60fdc1516a0a673746f2dd480
André Kirchner <andre.kirchner@cureon.de>
2019-04-01 16:36:52 +0200

    Restructured folders & added hygiene

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AdminModule } from './_core/admin/admin.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AdminModule);
   await app.listen(3000);
 }
 bootstrap();
